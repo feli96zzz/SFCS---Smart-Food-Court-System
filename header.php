fff4ef32238f4a95a8251ee40b78689a988b32fb
feli96zzz <feli96zzz@gmail.com>
2020-06-23 16:03:02 +0700

    sua header.php o local

--- header.php
+++ header.php
@@ -11,7 +11,7 @@ session_start();
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-		<title>Online Shopping</title>
+		<title>SFCS - Smart Food Court System</title>
 
 		<!-- Google font -->
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
@@ -122,12 +122,12 @@ session_start();
 			<div id="top-header">
 				<div class="container">
 					<ul class="header-links pull-left">
-						<li><a href="#"><i class="fa fa-phone"></i> +91-9535688928</a></li>
-						<li><a href="#"><i class="fa fa-envelope-o"></i> puneethreddy951@gmail.com</a></li>
-						<li><a href="#"><i class="fa fa-map-marker"></i>Bangalore</a></li>
+						<li><a href="#"><i class="fa fa-phone"></i> 1900 9001</a></li>
+						<li><a href="#"><i class="fa fa-envelope-o"></i> feedback@vn.SFCS.com</a></li>
+						<li><a href="#"><i class="fa fa-map-marker"></i>Ho Chi Minh</a></li>
 					</ul>
 					<ul class="header-links pull-right">
-						<li><a href="#"><i class="fa fa-inr"></i> INR</a></li>
+						<!-- kiem tra xung dot -->
 						<li><?php
                              include "db.php";
                             if(isset($_SESSION["uid"])){
@@ -179,7 +179,7 @@ session_start();
 							<div class="header-logo">
 								<a href="#" class="logo">
 								<font style="font-style:normal; font-size: 33px;color: aliceblue;font-family: serif">
-                                        Online Shop
+                                        SFCS
                                     </font>
 									
 								</a>
